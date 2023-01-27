d5ba486312fdc4e31fd664d934d938be656b5376
vanister <vanister@users.noreply.github.com>
2023-01-27 09:55:43 -0800

    Added a QRCodeImage component

---
+++ app/src/components/common/QrCodeImage.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import './QrCodeImage.scss';
+
+export type QrCodeImageProps = {
+  dataUri: string;
+  className?: string;
+  alt?: string;
+};
+
+export default function QrCodeImage(props: QrCodeImageProps) {
+  return (
+    <img
+      className={props.className || 'qr-code-image'}
+      alt={props.alt || 'QR Code Image'}
+      src={props.dataUri}
+    />
+  );
+}
