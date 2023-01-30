28e8f0732f7b77069a30c3deb6683bbf2639758d
vanister <vanister@users.noreply.github.com>
2023-01-29 16:16:38 -0800

    Stubbed mongo connection

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,10 +3,28 @@ require('dotenv').config();
 import express, { json } from 'express';
 import scanRouter from './routes/scan';
 import qrRouter from './routes/qr';
-
-// todo - connect to mongo
+import { connect } from 'mongoose';
 
 const app = express();
+
+// const {
+//   MATDO_MONGO_CONN_STR,
+//   MATDO_MONGO_SERVER,
+//   MATDO_MONGO_DB,
+//   MATDO_MONGO_UN,
+//   MATDO_MONGO_PW,
+// } = process.env;
+
+// // use the full connection string if one is set
+// if (MATDO_MONGO_CONN_STR) {
+//   connect(MATDO_MONGO_CONN_STR);
+// } else {
+//   connect(MATDO_MONGO_SERVER, {
+//     dbName: MATDO_MONGO_DB,
+//     user: MATDO_MONGO_UN,
+//     pass: MATDO_MONGO_PW,
+//   });
+// }
 
 // we accept only application/json data
 app.use(json());
