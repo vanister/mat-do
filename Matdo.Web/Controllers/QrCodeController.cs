2581d3c955377b4b0e9bb98e42bda1d0436eefc5
vanister <vanister@users.noreply.github.com>
2023-02-25 22:08:57 -0800

    Added stubbed QrCodeController

---
+++ Matdo.Web/Controllers/QrCodeController.cs
@@ -0,0 +1,21 @@
+using Matdo.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Matdo.Web.Controllers;
+
+[ApiController]
+[Route("qr")]
+public class QrCodeController : ControllerBase
+{
+  [HttpGet]
+  public IActionResult Get()
+  {
+    return Ok("hello world");
+  }
+
+  [HttpPost]
+  public IActionResult Create([FromBody] QrData data)
+  {
+    throw new NotImplementedException();
+  }
+}
---
+++ Matdo.Web/Models/QrData.cs
@@ -0,0 +1,7 @@
+namespace Matdo.Web.Models
+{
+  public class QrData
+  {
+    public Guid Id { get; set; }
+  }
+}
