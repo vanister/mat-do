a9eaeac3d72a1a55b039bffbf2503463d2d63981
vanister <vanister@users.noreply.github.com>
2023-03-03 20:22:47 -0800

    constrained Clone extension method

--- server/Matdo.Web/Utilities/Extensions.cs
+++ server/Matdo.Web/Utilities/Extensions.cs
@@ -2,7 +2,7 @@ namespace Matdo.Web.Utilties;
 
 public static class Extensions
 {
-    public static T Clone<T>(this T original, bool deep = false)
+    public static T Clone<T>(this T original, bool deep = false) where T : class
     {
         throw new NotImplementedException();
     }
