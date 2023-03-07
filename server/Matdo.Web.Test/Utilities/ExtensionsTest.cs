e5a5583a90223a656cee94fc4646c857cd5d7fce
vanister <vanister@users.noreply.github.com>
2023-03-07 13:28:39 -0800

    Added a Clone utility function

--- server/Matdo.Web.Test/Utilities/ExtensionsTest.cs
+++ server/Matdo.Web.Test/Utilities/ExtensionsTest.cs
@@ -1,2 +1,32 @@
+using Matdo.Web.Models;
+using Matdo.Web.Utilties;
+using MongoDB.Bson;
+
 namespace Matdo.Web.Test.Utilties;
 
+[TestClass]
+public class ExtensionsTesn
+{
+    [TestMethod]
+    public void Should_Clone_Success()
+    {
+        var item = new Item
+        {
+            Id = new ObjectId(),
+            Name = "Blaster",
+            CreatedAt = DateTime.Parse("12/31/2022"),
+            UserId = "string-representation-or-mongo-obj-id",
+            Description = "Jyn's blaster"
+        };
+
+        var cloned = item.Clone();
+
+        Assert.AreNotEqual(item, cloned);
+
+        Assert.AreEqual(item.Id, cloned.Id);
+        Assert.AreEqual(item.Description, cloned.Description);
+        Assert.AreEqual(item.Name, cloned.Name);
+        Assert.AreEqual(item.CreatedAt, cloned.CreatedAt);
+        Assert.AreEqual(item.UserId, cloned.UserId);
+    }
+}
--- server/Matdo.Web/Utilities/Extensions.cs
+++ server/Matdo.Web/Utilities/Extensions.cs
@@ -1,9 +1,21 @@
+using System.Reflection;
+
 namespace Matdo.Web.Utilties;
 
 public static class Extensions
 {
     public static T Clone<T>(this T original, bool deep = false) where T : class
     {
-        throw new NotImplementedException();
+        var type = typeof(T);
+        var cloned = Activator.CreateInstance<T>();
+
+        foreach (var property in type.GetProperties())
+        {
+            var value = property.GetValue(original);
+
+            property.SetValue(cloned, value);
+        }
+
+        return cloned;
     }
 }
