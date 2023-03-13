83e75f12076ada0bd2171fb20c9e35ecf660c8ad
vanister <vanister@users.noreply.github.com>
2023-03-12 22:18:33 -0700

    Updated routes

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -7,6 +7,7 @@ import Home from './pages/home/Home';
 import RequireAuth from './components/auth/RequireAuth';
 import PublicLayout from './pages/PublicLayout';
 import Scan from './pages/scan/Scan';
+import Dashboard from './pages/dashboard/Dashboard';
 
 export default function App() {
   return (
@@ -22,7 +23,7 @@ export default function App() {
           </RequireAuth>
         }
       >
-        {/* <Route path="/dashboard" element={<Home />} /> */}
+        <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/create" element={<Create />} />
         <Route path="*" element={<NotFound />} />
       </Route>
--- app/src/index.tsx
+++ app/src/index.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { Auth0Provider } from '@auth0/auth0-react';
-import reportWebVitals from './reportWebVitals';
 import App from './App';
 
 import './index.scss';
@@ -15,8 +14,8 @@ root.render(
   <React.StrictMode>
     <BrowserRouter>
       <Auth0Provider
-        domain={process.env.REACT_APP_AUTH0_DOMAIN}
-        clientId={process.env.REACT_APP_AUTH0_CLIENT_ID}
+        domain={process.env.REACT_APP_AUTH_DOMAIN}
+        clientId={process.env.REACT_APP_AUTH_CLIENT_ID}
         authorizationParams={{
           redirect_uri: window.location.origin
         }}
@@ -26,8 +25,3 @@ root.render(
     </BrowserRouter>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
