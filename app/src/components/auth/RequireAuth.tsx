2e3894f003fb282962a1980cd091ff378cfcc1c9
vanister <vanister@users.noreply.github.com>
2023-03-13 09:52:08 -0700

    Moved loading component

--- app/src/components/auth/RequireAuth.tsx
+++ app/src/components/auth/RequireAuth.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
-import Loading from '../Loading';
+import Loading from '../loading/Loading';
 
 export type RequireAuthProps = {
   children: React.ReactElement;
