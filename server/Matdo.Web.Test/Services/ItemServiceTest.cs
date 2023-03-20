ecda0bfdb326fffe2c84ca27450f0baf6d4b8e50
vanister <vanister@users.noreply.github.com>
2023-03-20 08:33:00 -0700

    Added simple ItemService test

--- server/Matdo.Web.Test/Services/ItemServiceTest.cs
+++ server/Matdo.Web.Test/Services/ItemServiceTest.cs
@@ -1,11 +1,34 @@
+using Matdo.Web.Models;
+using Matdo.Web.Repositories;
+using Matdo.Web.Services;
+using Moq;
+
 namespace Matdo.Web.Test.Services;
 
 [TestClass]
 public class ItemServiceTest
 {
     [TestMethod]
-    public void ListAsync_Should_Succeed()
+    public void Should_Instantiate_Service_Success()
     {
-        Assert.Fail("todo");
+        var service = SetupService();
+
+        Assert.IsNotNull(service);
+    }
+
+    private IItemService SetupService(IItemRepository? itemRepo = null)
+    {
+        var mockItemRepo = itemRepo ?? CreateItemRepoMock().Object;
+        var service = new ItemService(mockItemRepo);
+
+        return service;
+    }
+
+    private Mock<IItemRepository> CreateItemRepoMock()
+    {
+        var mockItemRepo = new Mock<IItemRepository>();
+        mockItemRepo.Setup(x => x.CreateAsync(It.IsAny<Item>())).ReturnsAsync(new Item());
+
+        return mockItemRepo;
     }
 }
