6b3ece5a022ada07cd4854440020a00293378d19
vanister <vanister@users.noreply.github.com>
2023-03-30 14:29:52 -0700

    Added a custom implementation of json-server to mimic matdo's backend api

---
+++ app/data/server.ts
@@ -0,0 +1,44 @@
+import * as jsonServer from 'json-server';
+import { resolve } from 'path';
+
+const server = jsonServer.create();
+const dbjsonPath = resolve('./data/db.json');
+const router = jsonServer.router(dbjsonPath);
+const middlewares = jsonServer.defaults();
+
+console.log('db.json path:', dbjsonPath);
+
+server.use(middlewares);
+server.use(jsonServer.bodyParser);
+
+server.use((req, res, next) => {
+  if (req.method === 'POST') {
+    req.body.createdAt = new Date().toISOString();
+  }
+
+  next();
+});
+
+// intercept and handle how matdo would send responses back
+server.use(({ path, method }, res, next) => {
+  const oldSend = res.send;
+
+  res.send = function (data: any) {
+    if (path.startsWith('/items') && method === 'POST') {
+      const item = JSON.parse(data);
+
+      oldSend.call(res, item?.id.toString());
+      return;
+    }
+
+    oldSend.call(res, data);
+  } as any;
+
+  next();
+});
+
+server.use(router);
+
+server.listen(3030, () => {
+  console.log('JSON Server is running at:', 'http://localhost:3030');
+});
