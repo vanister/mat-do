f4ca1a159958915d6c0fa140e8bb56eb1bba7cce
vanister <vanister@users.noreply.github.com>
2023-03-31 12:24:57 -0700

    Added a useScannedInfo hook

---
+++ app/src/hooks/useScannedInfo.ts
@@ -0,0 +1,21 @@
+import { useMemo } from 'react';
+import { useLocation } from 'react-router-dom';
+import { Item } from '../models/item';
+import { getScanItemInfo } from '../utilities/item-util';
+
+export default function useScannedInfo() {
+  const { hash } = useLocation();
+
+  const item = useMemo<Partial<Item>>(() => {
+    try {
+      const converted = getScanItemInfo(hash);
+
+      return converted;
+    } catch (error) {
+      console.warn('Failed to get the item info from the hash fragment');
+      return null;
+    }
+  }, [hash]);
+
+  return item;
+}
