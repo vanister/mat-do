c43552760f8042f0a3d826a68fe4276966da3cff
vanister <vanister@users.noreply.github.com>
2023-03-31 12:25:33 -0700

    Tested getting partial scanned item from hash frag

--- app/src/utilities/item-util.test.ts
+++ app/src/utilities/item-util.test.ts
@@ -1,5 +1,34 @@
-import { describe, expect, test, beforeEach } from '@jest/globals';
+import { describe, expect, test } from '@jest/globals';
+import { Item } from '../models/item';
+
+import { getScanItemInfo } from './item-util';
 
 describe('Item Utilities', () => {
-  test('should...', () => {});
+  const hash =
+    '#info=eyJuYW1lIjoiSnluJ3MgQmxhc3RlciIsImRlc2NyaXB0aW9uIjoiVGhlIG9uZSBzaGUgc3RvbGUgZnJvbSBDYXNzaWFuIn0=';
+
+  const item: Partial<Item> = {
+    name: `Jyn's Blaster`,
+    description: 'The one she stole from Cassian'
+  };
+
+  describe('WHEN getting a scanned item info', () => {
+    test('should return a partial Item', () => {
+      const scannedItem = getScanItemInfo(hash);
+
+      expect(scannedItem).toEqual(item);
+    });
+
+    test(`should throw an error if 'info' param is missing`, () => {
+      const badHash = '#notinfo=somebase64encodedstring';
+
+      expect(() => getScanItemInfo(badHash)).toThrowError('Missing info param');
+    });
+
+    test('should throw if hash is falsy', () => {
+      expect(() => getScanItemInfo('')).toThrowError('hash');
+      expect(() => getScanItemInfo(null)).toThrowError('hash');
+      expect(() => getScanItemInfo(undefined)).toThrowError('hash');
+    });
+  });
 });
--- app/src/utilities/item-util.ts
+++ app/src/utilities/item-util.ts
@@ -1,5 +1,6 @@
+import { url } from 'inspector';
 import { Item } from '../models/item';
-import { toBase64 } from './base64-util';
+import { fromBase64, toBase64 } from './base64-util';
 
 /**
  * Converts a given Item to an absolute URL string.
@@ -22,4 +23,31 @@ export function toScannableItemUrl(item: Item, path = '/scan'): string {
   // create a url with name and and desc embedded in the hash
   // so we don't have to retrieve it when it is scanned
   return `${window.location.origin}${path}/${item.id}#info=${encoded}`;
+}
+
+/**
+ * Converts a base64 encoded info string into a partial Item.
+ *
+ * @param hash The hash fragment containing the base64 encoded info string.
+ */
+export function getScanItemInfo(hash: string): Partial<Item> {
+  if (!hash) {
+    throw new Error('hash');
+  }
+
+  if (!hash.includes('info=')) {
+    throw new Error('Missing info param');
+  }
+
+  const params = new URLSearchParams(hash.replace('#', ''));
+
+  if (!params.has('info')) {
+    throw new Error('Missing info param');
+  }
+
+  const base64InfoStr = params.get('info');
+  const decodedInfoStr = fromBase64(base64InfoStr);
+  const item: Partial<Item> = JSON.parse(decodedInfoStr);
+
+  return item;
 }
