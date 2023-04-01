1bd48dd6b9ed72e97e9b4a1e319153cb19bafdb1
vanister <vanister@users.noreply.github.com>
2023-03-31 20:22:58 -0700

    Updated item models

--- app/src/models/item.d.ts
+++ app/src/models/item.d.ts
@@ -1,6 +1,21 @@
 export type Item = {
   id?: string;
-  userId?: string;
   name: string;
+  userId?: string;
   description?: string;
+  scanned?: number;
+  scans?: Scan[];
+};
+
+export type Scan = {
+  scannedAt: string;
+  comments: string;
+  coordinates: ItemCoordinates;
+};
+
+export type ItemCoordinates = {
+  longitude: number;
+  latitude: number;
+  accuracy: number;
+  altitude: number;
 };
