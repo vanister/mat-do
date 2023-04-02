9de1deda82e7728d58578a6e637a0fdf193423f3
vanister <vanister@users.noreply.github.com>
2023-04-02 14:13:30 -0700

    Cleaned up dashboard component

--- app/src/pages/dashboard/Dashboard.tsx
+++ app/src/pages/dashboard/Dashboard.tsx
@@ -1,7 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Item } from '../../models/item';
+import { useItemService } from '../../services/hooks/useItemService';
 
 export default function Dashboard() {
+  const [items, setItems] = useState<Item[]>();
+  const itemService = useItemService();
+
+  useEffect(() => {
+    itemService.list().then(items => setItems(items));
+  }, [itemService]);
+
   return (
-    <div>Dashboard</div>
+    <div className="dashboard-page">
+      {items?.map(item => (
+        <div key={item.id} className='list-item'>
+          <p>
+            {item.name}
+          </p>
+          <p>
+            {item.description}
+          </p>
+        </div>
+      ))}
+    </div>
   )
 }
