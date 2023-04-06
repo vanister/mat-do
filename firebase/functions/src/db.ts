36822f386b7810b86a6c92a14dd1785a62b6d33f
vanister <vanister@users.noreply.github.com>
2023-04-06 14:48:32 -0700

    Updated db.ts

--- firebase/functions/src/db.ts
+++ firebase/functions/src/db.ts
@@ -1,9 +1,9 @@
 import admin from 'firebase-admin';
 import { CollectionReference } from 'firebase-admin/firestore';
 
-admin.initializeApp();
+const app = admin.initializeApp();
 
-export const firestore = admin.firestore();
+export const firestore = admin.firestore(app);
 
 export function getCollection<T>(path: string): CollectionReference<T> {
   const collectionRef = firestore.collection(path) as CollectionReference<T>;
