c301e7a6322b34c2158b40cc16d66b0e3c72d991
vanister <vanister@users.noreply.github.com>
2023-04-06 21:56:00 -0700

    Removed unused import

--- firebase/functions/src/items/item.controller.ts
+++ firebase/functions/src/items/item.controller.ts
@@ -1,7 +1,6 @@
 import { Response } from 'firebase-functions/v1';
 import { create, get, list } from './item.service';
 import { Request } from 'firebase-functions/v2/https';
-import { Item } from './item-type';
 
 export async function listByUserId(req: Request, res: Response) {
   const { userId } = req.query;
