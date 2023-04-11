53e5b9f1030e88c28a06cf9ac6f1f9274778a659
vanister <vanister@users.noreply.github.com>
2023-04-11 10:46:49 -0700

    Import cleanup

--- firebase/functions/src/items/item.controller.ts
+++ firebase/functions/src/items/item.controller.ts
@@ -1,7 +1,6 @@
 import { Response } from 'firebase-functions/v1';
 import { create, get, list } from './item.service';
 import { Request } from 'firebase-functions/v2/https';
-import { FieldRequiredError } from '../errors/field-required.error.ts';
 import { BaseError } from '../errors/base.error';
 
 export async function listByUserId(req: Request, res: Response): Promise<void> {
