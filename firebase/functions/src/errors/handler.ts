e5731455cd31b72a95c33fde38ff422920ed3fdf
vanister <vanister@users.noreply.github.com>
2023-04-14 11:08:44 -0700

    Updated error handler to use firebase function logger

--- firebase/functions/src/errors/handler.ts
+++ firebase/functions/src/errors/handler.ts
@@ -1,11 +1,12 @@
 import { Response } from 'express';
 import { BaseError } from './base.error';
+import { logger as functionLogger } from 'firebase-functions/v2';
 import { Logger } from '../core';
 
 export function handleError(
   error: Error,
   response: Response,
-  logger: Logger = console
+  logger: Logger = functionLogger
 ): void {
   logger.log(error);
 
