d2126a5f60718904f777a1d47cbbc3b702573e58
vanister <vanister@users.noreply.github.com>
2023-04-19 13:48:29 -0700

    Moved the react app into the firebase dir

--- firebase/app/src/index.tsx
+++ firebase/app/src/index.tsx
@@ -14,7 +14,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <FirebaseAppProvider firebaseConfig={appSettings}>
+      <FirebaseAppProvider firebaseConfig={appSettings.firebaseOptions}>
         <App />
       </FirebaseAppProvider>
     </BrowserRouter>
