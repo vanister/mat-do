dfb5237a75d14a2542055ad0c91f0224fc6296f9
vanister <vanister@users.noreply.github.com>
2023-04-20 09:36:29 -0700

    Added a disclaimer to the home page

--- firebase/app/src/pages/home/Home.tsx
+++ firebase/app/src/pages/home/Home.tsx
@@ -8,9 +8,22 @@ export default function Home() {
     <div className="home-page">
       <Title>Home</Title>
       <p>
-        TODO: explain the benefits of creating a an account to generate qr codes
-        to help track your things
+        Generate QR Codes with a name and description and tag it to an item you
+        want to track using crowd sourcing.
       </p>
+      <p>
+        This low-tech solution depends on kind people who will scan and report
+        where your lost items are found ❤️.
+      </p>
+      <footer>
+        <p>
+          <em>
+            <strong>DISCLAIMER:</strong>
+          </em>{' '}
+          This is a demo, proof-of-concept, product. Please use at your own risk
+          as no guarantees are made about the integrity of the system.
+        </p>
+      </footer>
     </div>
   );
 }
