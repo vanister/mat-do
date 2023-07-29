749489fc41f44bad27066a6ae76a1af4b97f4073
vanister <vanister@users.noreply.github.com>
2023-07-29 07:24:40 -0700

    Updated form usage

--- firebase/app/src/pages/create/Create.tsx
+++ firebase/app/src/pages/create/Create.tsx
@@ -1,3 +1,5 @@
+import './Create.scss';
+
 import React, { useLayoutEffect, useReducer } from 'react';
 import Title from '../../components/Title';
 import QrCodeImage from '../../components/common/QrCodeImage';
@@ -11,9 +13,9 @@ import {
 } from './actions';
 import { CreateState } from './create-types';
 import { useItemService } from '../../hooks/services/useItemService';
-import Form, { FormAction, FormField } from '../../components/form/FormOld';
-
-import './Create.scss';
+import Form from '../../components/form/Form';
+import FormInput from '../../components/form/FormInput';
+import FormAction from '../../components/form/FormAction';
 
 const initialState: CreateState = {
   created: false,
@@ -33,39 +35,6 @@ export default function Create() {
     }
   }, [error]);
 
-  const fields: FormField[] = [
-    {
-      name: 'name',
-      label: 'Name',
-      value: name,
-      placeholder: `Jyn's blaster...`,
-      required: true,
-      onChange: (value) => {
-        dispatch(updateName(value));
-      }
-    },
-    {
-      name: 'description',
-      label: 'Description',
-      value: desc,
-      placeholder: 'The one she stole from Cassian...',
-      onChange: (value) => {
-        dispatch(updateDescription(value));
-      }
-    }
-  ];
-
-  const actions: FormAction[] = [
-    { text: 'Create', type: 'submit' },
-    {
-      text: 'Clear',
-      type: 'button',
-      onAction: () => {
-        dispatch(init);
-      }
-    }
-  ];
-
   async function handleFormSubmit(e: React.FormEvent) {
     e.preventDefault();
 
@@ -82,12 +51,43 @@ export default function Create() {
       <Title>Create an Item</Title>
       <section className="create-page-content">
         {qrCreated && <QrCodeImage dataUri={dataUri} />}
-        <Form
-          id="qrCodeForm"
-          fields={fields}
-          actions={actions}
-          onSubmit={handleFormSubmit}
-        />
+        <Form>
+          <FormInput
+            id="nameField"
+            label="Name"
+            value={name}
+            onChange={(value) => {
+              dispatch(updateName(value));
+            }}
+            additionalProps={{ placeholder: `Jyn's Blaster`, required: true }}
+          />
+          <FormInput
+            id="descriptionField"
+            label="Description"
+            value={desc}
+            multiline
+            onChange={(value) => {
+              dispatch(updateDescription(value));
+            }}
+            additionalProps={{ placeholder: `The one she stole from Cassian` }}
+          />
+          <FormAction
+            id="createButton"
+            type="submit"
+            onClick={handleFormSubmit}
+          >
+            Create
+          </FormAction>
+          <FormAction
+            id="clearButton"
+            type="button"
+            onClick={() => {
+              dispatch(init);
+            }}
+          >
+            Clear
+          </FormAction>
+        </Form>
       </section>
     </div>
   );
