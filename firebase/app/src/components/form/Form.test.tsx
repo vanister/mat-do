f5d7d97207945a2829c1c287751c626d2f63548a
vanister <vanister@users.noreply.github.com>
2024-03-25 22:11:41 -0700

    Added a stubbed Form test

---
+++ firebase/app/src/components/form/Form.test.tsx
@@ -0,0 +1,9 @@
+import { describe, expect, test, beforeEach } from '@jest/globals';
+
+describe('Form Tests', () => {
+  test.todo('should render a basic form');
+  test.todo('should handle submit actions');
+  test.todo('should handle normal actions');
+  test.todo('should render a single line input');
+  test.todo('should render a multiline input');
+});
