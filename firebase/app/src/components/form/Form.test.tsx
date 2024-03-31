8faa1cbb910b9ee95595598aa3fc9741b6904635
vanister <vanister@users.noreply.github.com>
2024-03-31 16:45:30 -0700

    Added tests for Form component

--- firebase/app/src/components/form/Form.test.tsx
+++ firebase/app/src/components/form/Form.test.tsx
@@ -1,5 +1,5 @@
 import '@testing-library/jest-dom';
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import Form from './Form';
 
 describe('Form Tests', () => {
@@ -16,11 +16,59 @@ describe('Form Tests', () => {
     expect(screen.getByRole('button')).toHaveTextContent('Ok');
   });
 
-  test('should handle submit actions', () => {});
+  test('should handle submit actions', () => {
+    const handleSubmit = jest.fn();
 
-  test.todo('should handle normal actions');
+    render(
+      <Form onSubmit={handleSubmit} additionalProps={{ 'aria-label': 'test-form' }}>
+        <Form.Input label="Name" value="Jyn" />
+        <Form.Action type="submit">Submit</Form.Action>
+      </Form>
+    );
 
-  test.todo('should render a single line input');
+    fireEvent.submit(screen.getByRole('form', { name: 'test-form' }));
 
-  test.todo('should render a multiline input');
+    expect(handleSubmit).toHaveBeenCalled();
+  });
+
+  test('should handle normal actions', () => {
+    const handleAction = jest.fn();
+
+    render(
+      <Form additionalProps={{ 'aria-label': 'test-form' }}>
+        <Form.Input label="Name" value="Jyn" />
+        <Form.Action type="button" onClick={handleAction}>
+          Ok
+        </Form.Action>
+      </Form>
+    );
+
+    fireEvent.click(screen.getByText('Ok'));
+
+    expect(handleAction).toHaveBeenCalled();
+  });
+
+  test('should render a single line input', () => {
+    render(
+      <Form additionalProps={{ 'aria-label': 'test-form' }}>
+        <Form.Input label="Name" value="Jyn" />
+      </Form>
+    );
+
+    const textInput = screen.getByRole('textbox');
+
+    expect(textInput).toHaveAttribute('type', 'text');
+  });
+
+  test('should render a multiline input', () => {
+    render(
+      <Form additionalProps={{ 'aria-label': 'test-form' }}>
+        <Form.Input multiline label="Name" value="Jyn" />
+      </Form>
+    );
+
+    const textInput = screen.getByRole('textbox');
+
+    expect(textInput).toBeInstanceOf(HTMLTextAreaElement);
+  });
 });
